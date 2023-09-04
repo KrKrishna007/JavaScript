4bcaa5e19aadefaaa2961c47efcb79d1ee70e962
Kumar Krishna <103742415+KrKrishna007@users.noreply.github.com>
2023-09-04 16:58:03 +0000

    data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version of js
+// alert(2+2) //we are using nodejs, not browser  engine
+
+console.log(3
+    +
+3); // code readability should be high dont write like this
+
+console.log("Krishna")
+
+let name = "Krishna"
+let age=21
+let isLoggedIn=false
+let state;
+
+// Primitive data types
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
